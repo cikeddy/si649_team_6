f13194cf8b88369dfe495fa82a9c108c29ef8905
Cassie Eddy <cikeddy@umich.edu>
2020-04-07 19:21:22 -0400

    adding demo

---
+++ assets/js/my.js
@@ -0,0 +1,33 @@
+<button onclick="one()">Healthcare Access</button>
+<button onclick="one()">Healthcare Utilization</button>
+<button onclick="one()">Perceived Health Status</button>
+<button onclick="one()">Health Outcomes</button>
+
+<script>
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a "loop" variable
+var i;
+
+// Full-width images
+function one() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "100%";
+  }
+}
+
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "50%";
+  }
+}
+
+// Four images side by side
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "25%";
+  }
+}
+</script>
